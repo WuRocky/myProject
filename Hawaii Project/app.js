b13af9ec68dabc0fef8d47e329ac99cabc0ae987
Rocky Wu <rockywu971@gmail.com>
2022-08-16 16:13:05 +0800

    2022/08/16 16:13

--- Hawaii Project/app.js
+++ Hawaii Project/app.js
@@ -1,16 +1,23 @@
 let header = document.querySelector("header");
 let headerAnchor = document.querySelectorAll("header nav ul li a");
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset != 0) {
-        header.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-        header.style.color = "white";
-        headerAnchor.forEach((a) => {
-            a.style.color = "white";
-        });
-    } else {
-        header.style = "";
-        headerAnchor.forEach((a) => {
-            a.style.color = "#09777d";
-        });
-    }
+	if (window.pageYOffset != 0) {
+		header.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+		header.style.color = "white";
+		headerAnchor.forEach((a) => {
+			a.style.color = "white";
+		});
+	} else {
+		header.style = "";
+		headerAnchor.forEach((a) => {
+			a.style.color = "#09777d";
+		});
+	}
 });
+
+// document.querySelector("#contact-form").addEventListener("submit", (e) => {
+// 	e.preventDefault();
+// 	e.target.elements.name.value = "";
+// 	e.target.elements.email.value = "";
+// 	e.target.elements.message.value = "";
+// });
